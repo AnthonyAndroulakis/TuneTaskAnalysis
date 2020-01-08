4809fdb434131f7547c1b24b8b80be74078185f9
Anthony Androulakis <aandroulakis@zoho.com>
2020-01-08 17:38:32 -0500

    Update analyses.py

--- analyses.py
+++ analyses.py
@@ -13,11 +13,9 @@ for i in list(os.walk('./TuneTask/Repetition'))[0][1]: #for each participant
     #now do repetition analysis
     os.system('echo tune_num,note_interval_error,rhythm_error,note_difference > ./'+i+'/Repetition/'+i+'.csv')
     for j in range(55,115): #for each wav file in each participant
-        #filename_name = j.split('/')[-1].split(".")[0] #the filename name
         tunenumber = str(j)
         filename_name = i+'_'+tunenumber
         filename = './TuneTask/Repetition/'+i+'/'+filename_name+'.wav'
-        #tunenumber = ''.join((ii if ii in '0123456789' else ' ') for ii in j).split()[-1] #tune number as a string
         os.system('ffmpeg -i '+filename+' -vn -ar 44100 -ac 2 -b:a 192k ./'+i+'/Repetition/mp3/'+filename_name+'.mp3') #convert to mp3 and save in repetition/mp3 folder
         SMM('./'+i+'/Repetition/mp3/'+filename_name+'.mp3') #saves file in tunetaskanalyses directory
         os.system('mv '+filename_name+'.txt ./'+i+'/Repetition/matricies/'+filename_name+'.txt') #move to correct directory
